f7915a86b9d8eefe0d20a8d41b97aa5bc7a30cdd
Alexander Samokhvalov <alexshikari9333@gmail.com>
2023-01-12 01:38:52 +0300

    Added cli args: response delay, folder path and logs

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+import argparse
 import os
 from pathlib import Path
 import logging
@@ -10,13 +11,13 @@ import asyncio
 logger = logging.getLogger(__file__)
 
 
-async def archive(request):
+async def archive(request, response_delay, file_folder_path):
     response = web.StreamResponse()
     response.headers['Content-Type'] = 'application/octet-stream'
     response.headers['Content-Disposition'] = 'attachment; filename="photos.zip"'
     archive_hash = request.match_info.get('archive_hash')
     root_path = Path(__file__).parent.resolve()
-    photos_root_path = os.path.join(root_path, 'test_photos')
+    photos_root_path = os.path.join(root_path, file_folder_path)
 
     if archive_hash is None or not os.path.exists(os.path.join(photos_root_path, archive_hash)):
         return web.HTTPNotFound(body='The archive does not exist or has been deleted')
@@ -27,7 +28,7 @@ async def archive(request):
         'zip', '-r', '-', '.',
         stdout=asyncio.subprocess.PIPE,
         stderr=asyncio.subprocess.PIPE,
-        cwd=os.path.join(Path(__file__).parent.resolve(), 'test_photos', archive_hash)
+        cwd=os.path.join(Path(__file__).parent.resolve(), file_folder_path, archive_hash)
     )
 
     chunk_size_in_bytes = 100000
@@ -38,6 +39,9 @@ async def archive(request):
             chunk_content = await proc.stdout.read(chunk_size_in_bytes)
             logging.info('Sending archive chunk ...')
             await response.write(chunk_content)
+
+            if response_delay:
+                await asyncio.sleep(response_delay)
 
     except (asyncio.CancelledError, web.HTTPRequestTimeout):
         logging.info(f'The download was interrupted')
@@ -55,11 +59,52 @@ async def handle_index_page(request):
     return web.Response(text=index_contents, content_type='text/html')
 
 
-if __name__ == '__main__':
-    logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.INFO)
+def create_parser():
+    parser = argparse.ArgumentParser(
+        prog='Async download service',
+        description='The service allows you to download files asynchronously'
+    )
+    parser.add_argument(
+        '-l',
+        '--logs',
+        action='store_true',
+        help='Enable logging'
+    )
+    parser.add_argument(
+        '-d',
+        '--response_delay',
+        type=int,
+        default=0,
+        help='Set server response delay',
+    )
+    parser.add_argument(
+        '-p',
+        '--file_folder_path',
+        type=str,
+        required=True,
+        help='Path to file folder'
+    )
+    return parser
+
+
+def main():
+    parser = create_parser()
+    args = parser.parse_args()
+    logs, file_folder_path, response_delay = args.logs, args.file_folder_path, args.response_delay
+
+    if not os.path.exists(file_folder_path):
+        raise FileNotFoundError('You specified a path to the folder that does not exist')
+
+    if logs:
+        logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.INFO)
+
     app = web.Application()
     app.add_routes([
         web.get('/', handle_index_page),
-        web.get('/archive/{archive_hash}/', archive),
+        web.get('/archive/{archive_hash}/', lambda request: archive(request, response_delay, file_folder_path)),
     ])
     web.run_app(app)
+
+
+if __name__ == '__main__':
+    main()
